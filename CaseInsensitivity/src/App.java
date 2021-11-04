216c8193ed9aa461445464b8e5574f9f94c4a9ac
widjoyono <abrorabyyu9@gmail.com>
2021-11-04 15:45:58 +0700

    init

--- CaseInsensitivity/src/App.java
+++ CaseInsensitivity/src/App.java
@@ -10,30 +10,26 @@ public class App {
 		Scanner scanner = new Scanner(System.in);
 		
 		boolean running = true;
-		
-		
+				
 		do {
 			System.out.println("Enter a command >");
 			String input = scanner.nextLine();
 			
-			if(input != 0 ) {
-				if(input.equalsIgnoreCase(START)) {
-					System.out.println("Machine starting...");
-				}
-				else if (input.equalsIgnoreCase(STOP)) {
-					System.out.println("Machine stopping ...");
-				}
-				else if (input.equalsIgnoreCase(QUIT)) {
-					running = false;
-				}
-				else {
-					System.out.println("unrecognized command");
-				}
+			if (input.length() == 0) {
+				continue;
+			}
+			
+			if(input.equalsIgnoreCase(START)) {
+				System.out.println("Machine starting...");
+			} else if (input.equalsIgnoreCase(STOP)) {
+				System.out.println("Machine stopping ...");
+			} else if (input.equalsIgnoreCase(QUIT)) {
+				running = false;
+			} else {
+				System.out.println("unrecognized command");
 			}
 		} while (running);
 		
 		scanner.close();
-		
 	}
-
 }
