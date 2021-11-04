f1657e4fc3a046e698dbbf429163680ab3bf5545
widjoyono <abrorabyyu9@gmail.com>
2021-11-04 17:43:33 +0700

    init

---
+++ ArraysOfString/src/App.java
@@ -0,0 +1,14 @@
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		String[] foods = {"Potatoes", "Rice", " Pasta", "Bread"};
+		
+		foods[1] = "Tomatoes";
+		
+		System.out.println(foods[0]);
+		System.out.println(foods[1]);
+		System.out.println(foods[3]);
+	}
+}
