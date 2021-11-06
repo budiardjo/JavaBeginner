713dfe51e937966bbf650261d0c2d267f2f5c002
widjoyono <abrorabyyu9@gmail.com>
2021-11-06 16:27:18 +0700

    init

---
+++ MountainTemperature/src/application/App.java
@@ -0,0 +1,13 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		var mountain1 = new Mountain(8100, 5, "K58");
+		var mountain2 = new Mountain(1200, 5, "K58");
+		
+		System.out.println(mountain1);
+		System.out.println(mountain2);
+	}
+
+}
--- WIndowExample/src/application/Window.java
+++ WIndowExample/src/application/Window.java
@@ -14,6 +14,38 @@ public class Window {
 	public int getWidth() {
 		return width;
 	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public boolean isVisible() {
+		return isVisible;
+	}
+
+	public void setVisible(boolean isVisible) {
+		this.isVisible = isVisible;
+	}
+
+	public int getTop() {
+		return top;
+	}
+
+	public void setTop(int top) {
+		this.top = top;
+	}
+
+	public int getLeft() {
+		return left;
+	}
+
+	public void setLeft(int left) {
+		this.left = left;
+	}
 	
 	
 }
