bf3b42eb023d436319127786db5e4082d1c7deca
widjoyono <abrorabyyu9@gmail.com>
2021-11-11 21:58:14 +0700

    init

--- HangMan/src/application/App.java
+++ HangMan/src/application/App.java
@@ -4,7 +4,8 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			Hangman game = new Hangman();
+			game.run();
 	}
 
 }
--- HangMan/src/application/Hangman.java
+++ HangMan/src/application/Hangman.java
@@ -1,5 +1,28 @@
 package application;
 
 public class Hangman {
-
+	
+	private boolean running = false;
+	private Randomword word = new Randomword();
+	
+	public void run() {
+		
+		do {
+			displayWord();
+			getUserInput();
+			checkUserInput();
+		} while (running);
+	}
+	
+	private void checkUserInput() {
+		System.out.println("checkUserInput");
+	}
+	
+	private void getUserInput () {
+		System.out.println("getUserInput");
+	}
+	
+	void displayWord() {
+		System.out.println(word);
+	}
 }
