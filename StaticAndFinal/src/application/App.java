53b7a23de1d1ed5970f0aa304578c3d38a28cbb7
widjoyono <abrorabyyu9@gmail.com>
2021-11-13 22:24:35 +0700

    commit init

---
+++ StaticAndFinal/src/application/App.java
@@ -0,0 +1,13 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		Cat cat1 = new Cat("Horace");
+		Cat cat2 = new Cat("MAtilda");
+		
+		System.out.println(cat1);
+		System.out.println(cat2);
+	}
+
+}
